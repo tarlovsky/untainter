302f744ca353ca08b9e344429794e00ae05cd73b
Oleksiy Tarlovskyy <tarlovsky@gmail.com>
2017-11-13 15:36:24 +0000

    HELLOOOO

--- codep.py
+++ codep.py
@@ -38,10 +38,8 @@ class Pattern:
         self.entry=entry[:]
         self.untaint_funcs=untaint_funcs[:]
         self.sinks=sinks[:]
-        
-    #def __str__(self):
-        #print("[*]"+self.name.join(map(str,self.entry)).join(map(str,self.untaint_funcs)).join(map(str,self.sinks)))
-        #print(map(str,self.entry))    
+
+     
 
 def init(names=None):
     global patterns, PATTERNS_FNAME, snippet_files, tainted
@@ -72,9 +70,8 @@ def init(names=None):
                 current_ast=json.load(current_tree) 
                 
                 for i, ch in enumerate(current_ast["children"]):
-                    print("*-----------Line: %d---------*" % i)
+                    print("\n*-----------Line: %d---------*" % i)
                     descend(ch)
-                    print("*---------------------------*")
                 print('\nTainted variables: '+str(tainted))
                 #cleanup for next file
                 
@@ -97,7 +94,8 @@ terminals=[
     u'string',
     u'constref',
     u'offsetlookup',
-    u'boolean'
+    u'boolean',
+    u'inline'
 ]
 
 def descend(child):
@@ -125,81 +123,137 @@ def descend(child):
             return eval(child['kind']+'_handle(child)') 
         elif(child['kind'] == 'if'):
             return eval(child['kind']+'_handle(child)')
+        elif(child['kind'] == 'echo'):
+            return eval('call_handle(child)')
         else:
             #switch funciton call here
-            routes=goto(child["kind"])
-            #print("KIND "+child['kind'])
-            tainted_varnames=[]
-            #print("ROUTES"+str(routes))
+            routes = goto(child["kind"])
+            
             for route in routes:
-                #might be a list of arguments #like in a function call;
-                #for each argument we descend;
+                #argument list?
                 if isinstance(child[route], list):
                     res = []
                     for i in range(0, len(child[route])):
-                        res.append(descend(child[route][i]))
+                        return res.append(descend(child[route][i]))
                 else:
-                    #offset lookup is definately here
-                    res = descend(child[route])
-                    #print('res',res)
-                    #print(res)
-                    for v in res:
-                        tainted_varnames.append(v) 
-            return tainted_varnames 
-
+                    #dives depper in cases line parenthesis->inner
+                    return descend(child[route])
+            
+            
     return None
 
-def taint_origin(var):
+def detuple(tup):
+    """ gets argument value from nested function calls """
+    """ (u'pg_escape_string', (u'mysql_real_escape_string', u'u1')) """
+    if isinstance(tup, tuple):
+        tup = tup[1]
+        return detuple(tup)
+    else:
+        return tup
+
+#returns original entry for a tainted variable
+#what if multiple entries
+def taint_origin(var, result):
     global tainted, entry_point_names
+    
+    # nested calls (u'pg_escape_string', (u'mysql_real_escape_string', u'u1'))
+    var = detuple(var)
 
-    for n in entry_point_names:   
-        #print(str(tainted)) 
-        
+    for n in entry_point_names:
         if n in '$'+var:
-            return var
-
-    for v in tainted[var]:
-        return taint_origin(v)
+            result.append(var)
+    
+    if var in tainted:
+        for v in tainted[var]:
+            taint_origin(v, result)
     
 
 def is_entry_point(s):
+    
+    s = detuple(s)
+    
+    
+    #if isinstance(s, tuple):
+        #s = s[1]
+        
     global entry_point_names
     for e in entry_point_names:
         if e in '$'+s:
             return 1
     return 0
 
+#origin array
 def is_sink(origin, id):
+    """ origin is a list """
+    """ need at least one match """
+    global patterns
     if None not in (origin,id):
+        if not isinstance(origin, list):
+            origin = [origin]
+        for o in origin:
+            if isinstance(o, tuple):
+                o = o[1]
+            found = 0
+            for p in patterns:
+                #has to be in same pattern
+                found = 0
+                for sink in p.sinks:
+                    if id == sink:
+                        found = found + 1
+                        break;
+                for e in p.entry:
+                    if e in '$'+o:
+                        found = found + 1
+                        break;
+                if found == 2:
+                    return True
+        return False
+
+#does this untaint function belong to the sensitive sink?
+def is_untaint_for_sink(untaint, sink):
+    """Does untaint fix sink"""
+    global patterns
+    if None not in (untaint, sink):
         found = 0
         for p in patterns:
-            #has to be in same pattern
             found = 0
-            for sink in p.sinks:
-                if id == sink:
+            for u in p.untaint_funcs:
+                if untaint == u:
                     found = found + 1
-            for e in p.entry:
-                if e in '$'+origin:
+                    break;
+            for s in p.sinks:
+                if s == sink:
                     found = found + 1
+                    break;
             if found == 2:
                 return True
     return False
 
+# TODO pass in sink-id
 def is_untaint(origin, id):
+    global patterns
+
     if None not in (origin,id):
-        found = 0
-        for p in patterns:
-            #has to be in same pattern
+        if not isinstance(origin, list):
+            origin = [origin]
+        for o in origin:
+            if isinstance(o, tuple):
+                o = o[1]
             found = 0
-            for u in p.untaint_funcs:
-                if id == u:
-                    found = found + 1
-            for e in p.entry:
-                if e in '$'+origin:
-                    found = found + 1
-            if found == 2:
-                return True
-    return False
+            for p in patterns:
+                #has to be in same pattern
+                found = 0
+                for u in p.untaint_funcs:
+                    if id == u:
+                        found = found + 1
+                        break;
+                for e in p.entry:
+                    if e in '$'+o:
+                        found = found + 1
+                        break;
+                if found == 2:
+                    return True
+        return False
 
 #returns list of cascaded tainted values
 def bin_handle(child):
@@ -216,9 +270,13 @@ def bin_handle(child):
     if lval is not None:
         lval = lval if isinstance(lval, list) else [lval]
         tmp.extend(lval)
-        
-    ret.extend([v for v in tmp if (v not in ret) and (v in tainted or is_entry_point(v))])
-        
+    
+    
+    #ret.extend([v for v in tmp if (v not in ret) and (v in tainted or is_entry_point(v))])
+    for v in tmp:
+        if isinstance(v,tuple) or (v not in ret and (v in tainted or is_entry_point(v))):
+            ret.append(v)
+    
     #we shall only return tainted values from the binary concatenation
     return ret
 
@@ -251,73 +309,182 @@ def encapsed_handle(child):
     return ret
 
 def call_handle(child):
-    id = descend(child['what'])
-    args = child['arguments']
-    original_entry = None
+    global tainted
 
+    #print("TAINTED: %s" % str(tainted))
+    
+    original_entry = None
+    args = child['arguments']
+    
+    
+    if child['kind'] == 'call':
+        id = descend(child['what'])
+    else:
+        #echo, printf
+        id = child['kind']
+    
     argv = []
+
+    
     for arg in args:
+        #print("%s ARGS inside : %s\n" % (id ,str(arg)))
         temp = descend(arg)
+        
         if isinstance(temp, list):
             argv.extend(temp)
         else:
             argv.append(temp)
-
+        
+        if temp and len(temp) == 0 and is_entry_point(temp):
+            tainted[temp] = []
     
-    if argv[0] is not None:
-        original_entry = taint_origin(argv[0])
+    #print("%s Descended ARGS %s " % (id, str(argv)))
 
-    #is argv[0] tainted
-    if argv[0] in tainted:
-        #is this a sink that is vulnerable to it's arg's original taint value? (i.e.: _GET, ..etc)
-        if is_sink(original_entry, id):
-            tainted_args=[]
-            for v in argv:
-                if v in tainted:
-                    tainted_args.append(v)
-            if len(tainted_args) > 0:
-                print('Sensitive sink [%s] is accepting a tainted value/s [%s]!' % (id, str(tainted_args)))
-                #return vals
+    if len(argv) > 0 and argv:
+        
+        # gets entry point for any variable
+        # returns entry point if argv[0] is entry point
+        
+        
+        taint_update = []  
+        ret=0
+
+            #is this a sink that is vulnerable to it's arg's original taint value? (i.e.: _GET, ..etc)
+        print(tainted)
+
+        for v in argv:
+            
+            original_entries=[]
+            
+            if v is not None:
+                #taint_origin detuples values
+                taint_origin(v, original_entries)
+            
+
+            if detuple(v) in tainted or is_entry_point(detuple(v)):    
                 
-        #does function untaint?
-        elif is_untaint(original_entry, id):
-            # untaint function calls block propagation of 
-            # taint values and return None
-            return None
-        return [argv[0]]
+                if original_entries and is_sink(original_entries, id):
+                    
+                    if v in tainted:
+                        for e in tainted[v]:
+                            ret += traverse(e, id)#are all of them clear?   
+                                
+                        # changed from ret == 2 * len(tainted[v]):
+                        #           to ret >= 2 * len(tainted[v]):
+                        # because of nested untaint functions
+                        if ret > 0 and ret >= 2 * len(tainted[v]):
+                            print('[*] @ Sensitive sink [%s] has beed corrected!' % (id))
+                        else:
+                            print('[*] @ Sensitive sink [%s] is accepting a tainted value/s [%s]!' % (id, str(argv[0])))
+                    # TODO Treat sensitive sinks after they've been processed.  
+                    if isinstance(v, tuple):
+                            taint_update.append( (id, v) ) 
+                    else:
+                        for t in tainted[v]:
+                            taint_update.append( (id, t) )
+                        
 
-    return [argv[0]]
+                #does function untaint any of the original entries?
+                elif is_untaint(original_entries, id):
+                    
+                    if isinstance(v, tuple):
+                        taint_update.append( (id, v) )
+                    else:
+                        for t in tainted[v]:
+                            taint_update.append( (id, t) ) 
+                        
+        
+        return taint_update
+            
+    print("RETURNINNNINGINGIGN")
+    return [argv]
+
+# return 1 on vulnerable.
+# traverse starting on a sensitive sink upword.
+def traverse(key, sink_id):
+    """returns 1 if not what we want, aka we reach any end and no fix"""
+    global tainted
+    
+    if isinstance(key, tuple):
+        # key tuple example
+        # (u'mysql_real_escape_string', u'_GET["username"]')  
+        
+        if is_untaint_for_sink(key[0], sink_id):
+            return 1 + traverse(key[1], sink_id)#without passing through tuple we won't untiant
+    
+        return traverse(key[1], sink_id)
+
+    elif isinstance(key, basestring):
+        
+        if is_entry_point(key):  
+            return 1
+            
+            #if is_sink(key, sink_id):
+                #return 1
+            
+        else:
+            for v in tainted[key]:
+                return traverse(v, sink_id)
+        
+    return 0
 
 def assign_handle(child):
     global tainted
     #we know we got an assign kind object
     rval = descend(child['right'])#this descent guarantees that it will populated tainted before lvalue check
     lval = descend(child['left'])
-    operator=child['operator']
+    operator = child['operator']
     
+
     if rval is not None:
         rval = rval if isinstance(rval, list) else [rval]
     if lval is not None:
         lval = lval if isinstance(lval, list) else [lval]
     
-    if rval is not None:
-        if operator == '.=':
-            for v in lval:
-                if v in tainted:
-                    rval = set().union(tainted[v], rval)
-
+    #   could be .= *= += -= &=
+    #   if is assign with an operator other than an '=' sign.
+    #   if operator != '=':
+    #
+    #   collect q into q
+    #   $q = $_GET["user"];
+    #   $q = $q.$u; 
+    
+    if operator != "=":
+        for v in lval:
+            if v in tainted and rval is not None:
+                rval = set().union(tainted[v], rval)
+    
+    if rval is not None:   
+        #take all right tainted values or entry points and add them to lval's list  
         tainted_by = []
+        
+        
         for v in rval:
-            if v and (v in tainted or is_entry_point(v)):
-                tainted_by.append(v)
             
+            #only propagate tainted values and tuples
+            if isinstance(v, tuple):
+                
+                tainted_by.append(v)
+            elif (v and v not in lval):
+                
+                if is_entry_point(v):
+                    tainted_by.append(v)
+                elif v in tainted:
+                    tainted_by.extend(tainted[v])
+                
+
+    
         if len(tainted_by) > 0:
+           
             tainted[lval[0]] = tainted_by
-        return lval
-
-
-        del tainted[lval[0]]
-
+            return lval
+        
+        #  we got here means no tainted values being assigned to it.
+        #  remove it from tanted list because
+        #  an assign to no tainted values rewrites it.
+        if lval[0] in tainted:
+            del tainted[lval[0]]     
+        
     return lval
 
 #returns if entry point like: $_GET['name'] doesnt return if $a[1] normal lookup
@@ -333,7 +500,6 @@ def offsetlookup_handle(child):
                 return p1+'["'+p2+'"]'
             else:
                 return p1+'['+p2+']'
-    print('here')
     return 0
 
 
--- index.js
+++ index.js
@@ -1,40 +1,38 @@
   // initialize the php parser factory class
-var fs = require('fs');
-var path = require('path');
-var engine = require('php-parser');
+  var fs = require('fs');
+  var path = require('path');
+  var engine = require('php-parser');
+  
+  // initialize a new parser instance
+  var parser = new engine({
+	  // some options :
+    parser: {
+      extractDoc: true
+    },
+    ast: {
+      withPositions: false
+    }
+  });
+  
+  // Retrieve the AST from the specified source
+  //var eval = parser.parseEval('echo "Hello World";');
+  
+  // Retrieve an array of tokens (same as php function token_get_all)
+  //var tokens = parser.tokenGetAll('<?php echo "Hello World";');
+  
+  // Load a static file (Note: this file should exist on your computer)
+  
 
-// initialize a new parser instance
-var parser = new engine({
-  // some options :
-  parser: {
-    extractDoc: true
-  },
-  ast: {
-    withPositions: false
-  }
-});
-
-// Retrieve the AST from the specified source
-//var eval = parser.parseEval('echo "Hello World";');
-
-// Retrieve an array of tokens (same as php function token_get_all)
-//var tokens = parser.tokenGetAll('<?php echo "Hello World";');
-
-// Load a static file (Note: this file should exist on your computer)
-
-var files=[];
-var fnames=[];
-
-process.argv.forEach(function (val, index, array) {
-  if(index > 1){
-    fnames.push(val)
-    files.push(fs.readFileSync( val, "utf-8" ));
-  }
-});
-// Log out results
-//console.log( 'Eval parse:', eval );
-//console.log( 'Tokens parse:', tokens );
-files.forEach(function(ele,i){
-  var res = parser.parseCode(ele)
-  fs.writeFileSync('ast_'+fnames[i], JSON.stringify(res, null, '\t'))
-});
+  process.argv.forEach(function (val, index, array) {
+    if(index > 1){    
+      fs.watch(val, { encoding: 'buffer' }, (eventType, filename) => {
+        if (filename) {
+          var content = fs.readFileSync( val, "utf-8" )
+          console.log("Writing changes: "+filename);
+          var res = parser.parseCode(content)
+          fs.writeFileSync('ast_'+val, JSON.stringify(res, null, '\t'))
+        }
+      });
+    }
+  });
+ 
